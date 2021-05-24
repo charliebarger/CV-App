e2bb123d5fa622c8697ebecfcdf1da3c17b7f175
charliebarger <charliebarger04@gmail.com>
2021-05-24 08:31:12 -0600

    add and style CV page

--- src/componenets/PrintCV.js
+++ src/componenets/PrintCV.js
@@ -1,17 +1,24 @@
 import React from "react";
+import "../App.css";
 
 const PrintCV = (props) => {
   const { values } = props;
   console.log(props.values);
   return (
-    <div>
-      <p>{values.firstName.text}</p>
-      <p>{values.lastName.text}</p>
-      <p>{values.title.text}</p>
-      <p>{values.address.text}</p>
-      <p>{values.phone.text}</p>
-      <p>{values.email.text}</p>
-      <p>{values.description.text}</p>
+    <div className="resume">
+      <div className="resume-header">
+        <p className="full-name">
+          {values.firstName.text + " " + values.lastName.text}
+        </p>
+        <p className="title">{values.title.text}</p>
+      </div>
+      <div className="resume-main"></div>
+      <div className="resume-details">
+        <p className="address">{values.address.text}</p>
+        <p className="phone">{values.phone.text}</p>
+        <p className="email">{values.email.text}</p>
+        <p className="description">{values.description.text}</p>
+      </div>
     </div>
   );
 };
