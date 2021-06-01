a28fc4a83f8c199c1fcaae87b567f6aef9a25c0c
charliebarger <charliebarger04@gmail.com>
2021-06-01 08:20:44 -0600

    rename files and variables

--- src/componenets/CV/PrintCV.js
+++ src/componenets/CV/PrintCV.js
@@ -1,31 +1,30 @@
 import React from "react";
 import "../../App.css";
-import CVheader from "./CVheader";
-import PrintExperience from "./PrintExperience";
+import CVheader from "./CVheaderSection";
+import PrintExperience from "./CVExperienceSection";
 
 const PrintCV = (props) => {
-  console.log(props);
-  const values = props.values.Personal.Inputs;
-  console.log(values);
+  const personal = props.values.Personal;
+  const experience = props.values.Experience;
   return (
     <div className="resume">
       <CVheader
-        firstName={values.firstName.text}
-        lastName={values.lastName.text}
-        title={values.title.text}
+        firstName={personal.Inputs.firstName.text}
+        lastName={personal.Inputs.lastName.text}
+        title={personal.Inputs.title.text}
       />
       <div className="resume-main">
         <h2 className="main-subsection">About</h2>
-        {/* <p>{props.values.Personal.description.text}</p> */}
-        <PrintExperience experiences={props.values.Experience} />
+        <p>{props.values.Personal.description.text}</p>
+        <PrintExperience experiences={experience} />
       </div>
       <div className="resume-details">
         <p className="details-subheader">Address</p>
-        <p className="address detail">{values.address.text}</p>
+        <p className="address detail">{personal.Inputs.address.text}</p>
         <p className="details-subheader">Phone Number</p>
-        <p className="phone detail">{values.phone.text}</p>
+        <p className="phone detail">{personal.Inputs.phone.text}</p>
         <p className="details-subheader">Email Address</p>
-        <p className="email detail">{values.email.text}</p>
+        <p className="email detail">{personal.Inputs.email.text}</p>
       </div>
     </div>
   );
